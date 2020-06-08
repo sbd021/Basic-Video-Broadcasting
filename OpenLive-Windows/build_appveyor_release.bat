465c3bfede78e74fafff1c56dfe001c17d8f5b48
sunbaodong <sbd021@126.com>
2020-06-08 14:20:49 +0800

    Update build_appveyor_release.bat

--- OpenLive-Windows/build_appveyor_release.bat
+++ OpenLive-Windows/build_appveyor_release.bat
@@ -4,9 +4,10 @@ title qmake and nmake build prompt
 set SDKFolderVersion=%~1
 set Machine=%~2
 set ProjName=%~3
+set WorkingDirectory=%~4
 echo SDKFolderVersion: %SDKFolderVersion%
 echo ProjName:%ProjName%
-
+echo WorkingDirectory=%WorkingDirectory%
 
 if %Machine% == x86 (
   set QTDIR=C:\Qt5.10.1\5.10.1\\msvc2017
@@ -17,7 +18,7 @@ if %Machine% == x86 (
 set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
 
 call "%VCINSTALLDIR%\vcvarsall.bat" %Machine%
-%QTDIR%\bin\qmake.exe %ProjName%.pro "CONFIG+=release" "CONFIG+=qml_release"
+%QTDIR%\bin\qmake.exe %WorkingDirectory%\%ProjName%.pro "CONFIG+=release" "CONFIG+=qml_release"
 nmake
 
 if not exist release (
@@ -34,11 +35,11 @@ cd ..
 
 set PackageDIR=%ProjName%_Win_v%SDKFolderVersion%
 if not exist %PackageDIR% (
-	 mkdir %PackageDIR%
+    mkdir %PackageDIR%
 )
 cd %PackageDIR%
 mkdir %Machine%
-xcopy /S /I ..\Release\*.* %Machine% /y
+xcopy /S /I %WorkingDirectory%\Release\*.* %Machine% /y
 xcopy /S /I ..\sdk\dll\*.* %Machine% /y
 cd ..
 rmdir /S /Q Release
